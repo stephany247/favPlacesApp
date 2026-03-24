1fadcfd71830b1d6667c4a24fbc2fc189a0b3109
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-03-24 13:50:09 +0100

    feat: add navigation setup with stack navigator

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 function PlacesList({ places }) {
   if (!places || places.length === 0) {
